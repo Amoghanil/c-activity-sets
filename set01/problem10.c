19c0785c1cb213c6a4e57b8f1ac86bcc6f07be9e
Amogh P A <amoghpa.bsc23@rvu.edu.in>
2024-02-12 14:12:02 +0530

    test

--- set01/problem10.c
+++ set01/problem10.c
@@ -10,7 +10,7 @@ void input_two_strings(char *string1, char *string2) {
 
 int stringcompare(char *string1, char *string2) {
     while(*string1 != '\0' && *string2 != '\0') {
-        if(*string1 != *string2) {
+        if( *string1 != *string2) {
             return -1;
         } else if(*string1 > *string2) {
             return 1;
