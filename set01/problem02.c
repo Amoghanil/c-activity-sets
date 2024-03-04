90736c4f062e177187f4d150ea40e06f7fb850c4
Amogh P A <amoghpa.bsc23@rvu.edu.in>
2024-03-04 13:56:57 +0530

    test

--- set01/problem02.c
+++ set01/problem02.c
@@ -2,7 +2,7 @@
 #include<stdio.h>
 int main () {
     int a , b, c;
-    printf("enter the 1st numbes:");
+    printf("enter the numbes:");
     scanf("%d""%d", &a, &b);
     c = a + b;
     printf("the sum of a and b is %d",c);
